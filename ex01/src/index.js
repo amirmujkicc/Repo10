4c3d07b2aa025fe62b80535119e5707c06a6aae1
Amir Mujkic <mujkicc@gmail.com>
2021-09-22 21:51:34 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var myPet = {
+    species: "bulldog",
+    name: "Bully",
+    legs: 4,
+    friends: ["cat", "parrot"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = {
+    myPet,
+    myFunction
+};
